df667d2a6bffbd79cf81e7f884d4fe8478159771
David Glodich <david.glodich@compuware.com>
2021-05-25 14:26:51 -0400

    ZENG-172871: Copyright corrections.

--- src/main/java/com/compuware/jenkins/build/JclDescriptorImpl.java
+++ src/main/java/com/compuware/jenkins/build/JclDescriptorImpl.java
@@ -1,3 +1,20 @@
+/**
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2018 Compuware Corporation
+ * (c) Copyright 2018, 2021 BMC Software, Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.compuware.jenkins.build;
 
 import java.util.Collections;
@@ -182,14 +199,3 @@ public abstract class JclDescriptorImpl<T extends BuildStep & Describable<T>> ex
 		return model;
 	}
 }
-
-/*******************************************************************************************************************************************
- * THESE MATERIALS CONTAIN CONFIDENTIAL INFORMATION AND TRADE SECRETS OF BMC SOFTWARE, INC. YOU SHALL MAINTAIN THE MATERIALS AS CONFIDENTIAL
- * AND SHALL NOT DISCLOSE ITS CONTENTS TO ANY THIRD PARTY EXCEPT AS MAY BE REQUIRED BY LAW OR REGULATION. USE, DISCLOSURE, OR REPRODUCTION
- * IS PROHIBITED WITHOUT THE PRIOR EXPRESS WRITTEN PERMISSION OF BMC SOFTWARE, INC.
- * 
- * ALL BMC SOFTWARE PRODUCTS LISTED WITHIN THE MATERIALS ARE TRADEMARKS OF BMC SOFTWARE, INC. ALL OTHER COMPANY PRODUCT NAMES ARE TRADEMARKS
- * OF THEIR RESPECTIVE OWNERS.
- * 
- * (c) Copyright 2021 BMC Software, Inc.
- ******************************************************************************************************************************************/
